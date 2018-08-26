956f05033628836129a62f8c43c01bdee0f9ffbc
alex2wong <huangyixiu188@hotmail.com>
2018-08-26 23:22:56 +0800

    update bundle

--- examples/canvasdemo.js
+++ examples/canvasdemo.js
@@ -3,30 +3,7 @@
 mapboxgl.accessToken = false;
 var mapCenter = [118.0066, 30.6135];
 var map = new mapboxgl.Map({
-    style: {
-        "version": 8,
-        "sprite": "https://alex2wong.github.io/mapbox-plugins/assets/sprite",
-        "glyphs": "https://alex2wong.github.io/mapbox-plugins/{fontstack}/{range}.pbf",
-        "sources": {
-            "custom-tms": {   
-                'type': 'raster',
-                'tiles': [
-                    // `https://huangyixiu.co:3003/proxy?proxyURI=http://map.geoq.cn/
-                    // ArcGIS/rest/services/ChinaOnlineStreetPurplishBlue/MapServer/tile/{z}/{y}/{x}`,
-                    "http://www.google.cn/maps/vt?lyrs=s@702&gl=cn&x={x}&y={y}&z={z}"
-                ],
-                'tileSize': 256
-            },
-        },
-        "layers": [
-            {
-                'id': 'custom-tms',
-                'type': 'raster',
-                'source': 'custom-tms',
-                'paint': {}
-            },
-        ]
-    },
+    style: Mapbox.Config.darkStyle,
     center: mapCenter,
     zoom: 7,
     pitch: 45,
